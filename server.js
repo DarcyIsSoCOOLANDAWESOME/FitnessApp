7c7a79ddfeaa24a9b9f911cf12724e63ea757cf7
‘darcy’ <darcy.mears@yahoo.com>
2026-08-02 21:04:11 +0100

    I can add a new user in postman

--- server.js
+++ server.js
@@ -23,12 +23,9 @@ const init = async () => {
       method: "GET",
       path: "/workout-exercise",
       handler: async () => {
-        console.log("STEP 1");
-
+        // console.log("STEP 1");
         await prisma.$connect();
-
-        console.log("STEP 2 - connected");
-
+        // console.log("STEP 2 - connected");
         const result = await prisma.$queryRaw`
       SELECT * FROM public."workoutExercise"
     `;
@@ -40,10 +37,45 @@ const init = async () => {
       },
     },
     {
+      method: "POST",
+      path: "/user",
+      handler: async (request, h) => {
+        const { name, email } = request.payload;
+
+        const user = await prisma.user.create({
+          data: {
+            name,
+            email,
+          },
+        });
+
+        return user;
+      },
+    },
+    {
+      method: "DELETE",
+      path: "/workout-exercise",
+      handler: async (request, h) => {},
+    },
+    {
       method: "GET",
       path: "/health",
       handler: () => {
         return "health status ok";
+      },
+    },
+
+    {
+      method: "GET",
+      path: "/exercises",
+      handler: async () => {
+        const response = await fetch(
+          "https://wger.de/api/v2/exercise/?language=2",
+        );
+
+        const data = await response.json();
+
+        return data;
       },
     },
   ]);
